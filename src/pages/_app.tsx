1a869492818d9cf7d4bacc02d76364fdc495884e
amusom9823 <136586740+amusom9823@users.noreply.github.com>
2023-09-04 23:10:16 +0900

    MUI導入②プロジェクトの枠組みにMUIを組み込む
    プロジェクトに組み込む
    ●参考
    https://github.com/mui/material-ui/tree/v5.12.3/examples/material-next-ts
    https://qiita.com/cabbage_lettuce/items/5294cb4d1caa0d6adc4a

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
-import '@/styles/globals.css'
-
+import type { EmotionCache } from '@emotion/react'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
 
+import createEmotionCache from '@/createEmotionCache'
+
+import theme from '../theme'
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
