a6c4cc7488cbf0323f136edcddded4aa44426d8f
amusom9823 <136586740+amusom9823@users.noreply.github.com>
2023-09-05 19:52:08 +0900

    tRPC導入③取得したデータを表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import { Box, Container, Typography } from '@mui/material'
 import { Inter } from 'next/font/google'
 
+import { trpc } from '@/utils/trpc'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const hello = trpc.hello.useQuery({ text: 'client' })
+  if (!hello.data) {
+    return <div>Loading...</div>
+  }
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -15,6 +22,9 @@ export default function Home() {
           </Typography>
           <Box mt={4}>discription</Box>
         </Box>
+        <div>
+          <p>{hello.data.greeting}</p>
+        </div>
       </Container>
     </main>
   )
