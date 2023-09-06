72dfee375d7c3865cc9b0d2e3ad1901eab60fa5d
amusom9823 <136586740+amusom9823@users.noreply.github.com>
2023-09-06 19:34:14 +0900

    ユーザー管理機能の追加①ユーザー登録画面の追加
    ユーザー登録画面を追加、入力フォームに情報を入力・基本的な検証を入れる
    (サーバー側への伝達はまだ)
    
    > npm install formik@2.4.3 yup@1.2.0

---
+++ src/pages/register.tsx
@@ -0,0 +1,89 @@
+import { Box, Button, Container, TextField } from '@mui/material'
+import { Form, Formik } from 'formik'
+import React from 'react'
+import * as Yup from 'yup'
+
+export default function RegisterForm() {
+  return (
+    <Container maxWidth="xs">
+      <Box>
+        <Formik
+          initialValues={{ username: '', email: '', password: '', error: null }}
+          onSubmit={(values, { setErrors }) => {
+            console.log(values)
+          }}
+          validationSchema={Yup.object({
+            username: Yup.string().required(),
+            email: Yup.string().required().email(),
+            password: Yup.string().required(),
+          })}
+        >
+          {({
+            handleSubmit,
+            isSubmitting,
+            errors,
+            isValid,
+            dirty,
+            values,
+            handleChange,
+            handleBlur,
+            touched,
+          }) => (
+            <Form
+              className="ui form error"
+              onSubmit={handleSubmit}
+              autoComplete="off"
+            >
+              <h3>Register</h3>
+
+              <TextField
+                margin={'normal'}
+                fullWidth={true}
+                id="username"
+                name="username"
+                label="User Name"
+                type="text"
+                value={values.username}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.username && Boolean(errors.username)}
+                helperText={touched.username && errors.username}
+              />
+
+              <TextField
+                margin={'normal'}
+                fullWidth={true}
+                id="email"
+                name="email"
+                label="Email"
+                type="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.email && Boolean(errors.email)}
+                helperText={touched.email && errors.email}
+              />
+
+              <TextField
+                margin={'normal'}
+                fullWidth={true}
+                id="password"
+                name="password"
+                label="password"
+                type="password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.password && Boolean(errors.password)}
+                helperText={touched.password && errors.password}
+              />
+              <Button type="submit" variant="contained">
+                Submit
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Box>
+    </Container>
+  )
+}
