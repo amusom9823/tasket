fd24eb8bb786684bf34dd4060014d840aeac47f8
amusom9823 <136586740+amusom9823@users.noreply.github.com>
2023-09-21 06:30:50 +0900

    ユーザー管理機能の追加➉認証状態をAPIでも把握する

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,10 +1,19 @@
 import type * as trpc from '@trpc/server'
 import type * as trpcNext from '@trpc/server/adapters/next'
+import { getServerSession } from 'next-auth'
+
+import { nextAuthOptions } from '@/pages/api/auth/[...nextauth]'
 
 import prisma from './prisma'
 
 export const createContext = async (ctx: trpcNext.CreateNextContextOptions) => {
+  const { req, res } = ctx
+  const session = await getServerSession(req, res, nextAuthOptions)
+
   return {
+    req,
+    res,
+    session,
     prisma,
   }
 }
--- src/server/routers/helloRouter.ts
+++ src/server/routers/helloRouter.ts
@@ -10,8 +10,15 @@ export const helloRouter = router({
       }),
     )
     .query((opts) => {
+      let userinfo = ''
+
+      if (opts.ctx.session) {
+        userinfo = opts.ctx.session.user?.name || ''
+      } else {
+        userinfo = 'Not signin'
+      }
       return {
-        greeting: `hello ${opts.input.text} !!!!!`,
+        greeting: `hello ${opts.input.text} !!!!! ${userinfo}`,
       }
     }),
   today: procedure.query(() => {
