d9dc2b28c4828c787fab62ea3a2bde9453ec2412
amusom9823 <136586740+amusom9823@users.noreply.github.com>
2023-09-21 06:24:42 +0900

    ユーザー管理機能の追加⑨クライアントでの認証状態に従いメニューを切り替える
    ログイン未→ログイン・登録リンク
    ログイン済み→メニューリスト表示・ログイン名・ログアウトボタン表示

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,31 +1,62 @@
-import { AppBar, Button, Toolbar, Typography } from '@mui/material'
+import {
+  AppBar,
+  Button,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from '@mui/material'
 import Link from 'next/link'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import { useState } from 'react'
 
 export default function NavBar() {
   const { data: session } = useSession()
 
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" style={{ flexGrow: 1 }}>
           Logo
         </Typography>
-        <Link href="/register">
-          <Button color="inherit">Register</Button>
-        </Link>
         <Link href="/">
           <Button color="inherit">Home</Button>
         </Link>
-
         {!session ? (
-          <Button color="inherit" onClick={() => signIn()}>
-            signIn
-          </Button>
+          <>
+            <Button color="inherit" onClick={() => signIn()}>
+              Login
+            </Button>
+            <Link href="/register">
+              <Button color="inherit">Register</Button>
+            </Link>
+          </>
         ) : (
-          <Button color="inherit" onClick={() => signOut()}>
-            signOut
-          </Button>
+          <>
+            <Button color="inherit" onClick={handleClick}>
+              {session?.user?.name}
+            </Button>
+
+            <Menu
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={() => signOut()}>SignOut</MenuItem>
+            </Menu>
+          </>
         )}
       </Toolbar>
     </AppBar>
